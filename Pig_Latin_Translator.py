d05c70c0b539c504b7f51d0f6f4cba98504fe400
JordanRobo <jordan@stateot.art>
2023-11-15 15:44:41 +1100

    Fixed translation so that it correctly adheres to Pig Latin Conventions Fixed issue where spacing and sentence structure where being stripped Fixed issue where capital letters were being moved and remaining capitalised Added an .exe version that can be run without needing to download Python

--- Pig_Latin_Translator.py
+++ Pig_Latin_Translator.py
@@ -3,15 +3,16 @@ from tkinter import filedialog
 from tkinter.font import Font
 import time
 
-# Variable to define vowels
+# Variables to define vowels and punctuation
 vowels = ('a', 'e', 'i', 'o', 'u')
 punct = (',', '.', '!', '?', ':', ';')
 
 def main():
     # Create the main window
     root = tk.Tk()
-    root.title("Pig Latin Generator")
+    root.title("Pig Latin Translator")
 
+    # Define fonts for various components
     title_font = Font(family="Helvetica", size=20, weight="bold")
     paragraph_font = Font(family="Helvetica", size=12)
     button_font = Font(family="Helvetica", size=12, weight="bold")
@@ -22,79 +23,85 @@ def main():
             title="Open Text File",
             filetypes=[("Text Files", "*.txt")]
         )
-        if not filename:  # No file was selected
+        if not filename:  # Check if a file was selected
             return
 
-        # If a file was selected Python will read the file contents
+        # Read the content of the file
         with open(filename, 'r') as file:
-            input_text = file.read()
+            lines = file.readlines()
 
-        def process_word(word):
-        # Check if the word ends with a punctuation mark
-            if word[-1] in punct:
-                # If it does, remove the punctuation and store it
-                punctuation = word[-1]
-                word = word[:-1]
-            else:
-                # If not, there's no punctuation to add later
-                punctuation = ''
+        # Process each line
+        processed_lines = []
+        for line in lines:
+            words = line.split()
+            processed_words = [process_word(word) for word in words]
+            processed_line = ' '.join(processed_words)
+            processed_lines.append(processed_line)
 
-            # Process the word so that it follows the Pig Latin rules
-            if word[0].lower() in vowels:
-                processed_word = word + 'ay'
-            else:
-                processed_word = word[1:] + word[0] + 'ay'
+        converted_text = '\n'.join(processed_lines)
 
-            # Return the processed word with the punctuation added back (if any)
-            return processed_word + punctuation
-        
+        time.sleep(2) # Wait for two seconds before proceeding
 
-        words = input_text.split()
-        p_latin = [process_word(word) for word in words]
-        converted_text = ' '.join(p_latin)
-
-        time.sleep(2) # Tells python to wait two seconds before saving the file
         # Ask where to save the converted file
         save_filename = filedialog.asksaveasfilename(
             title="Save Converted File",
             filetypes=[("Text Files", "*.txt")],
             defaultextension=".txt"
         )
-        # Once it has read and converted the document it will write the new converted one
         if save_filename:
             with open(save_filename, 'w') as file:
                 file.write(converted_text)
 
-        
-    # Configure the grid - set column weight
-    root.grid_columnconfigure(0, weight=1)  # This makes the column expand
+    # Function to process each word into Pig Latin
+    def process_word(word):
+        # Handling punctuation and capitalization
+        original_punctuation = ''
+        if word[-1] in punct:
+            original_punctuation = word[-1]
+            word = word[:-1]
+        is_capitalized = word[0].isupper()
+        word = word.lower()
 
-    # Configure the grid
+        # Finding the first vowel and processing the word
+        first_vowel_index = next((i for i, char in enumerate(word) if char in vowels), None)
+        if first_vowel_index is None:
+            processed_word = word  # No vowel found
+        elif first_vowel_index == 0:
+            processed_word = word + 'way'
+        else:
+            processed_word = word[first_vowel_index:] + word[:first_vowel_index] + 'ay'
+
+        # Reapplying capitalization
+        if is_capitalized:
+            processed_word = processed_word.capitalize()
+
+        return processed_word + original_punctuation
+
+    # GUI layout configuration
     root.grid_columnconfigure(0, weight=1)
-    for i in range(6):  # Creates 6 Rows
-        root.grid_rowconfigure(i, weight=1)  # This makes the row expand
+    for i in range(6):  # Configuring rows
+        root.grid_rowconfigure(i, weight=1)
 
-    # Application Heading
-    title = tk.Label(root, text="Welcome to Pig Latin Text Converter", font=title_font, bg="#3B6869", fg="#ffffff")
+    # GUI components
+    title = tk.Label(root, text="Welcome to the Pig Latin Translator", font=title_font, bg="#3B6869", fg="#ffffff")
     title.grid(row=1, column=0, sticky="NSEW")
-
-    description_text = tk.Label(root, text="Using this program you are able to seamlessly convert a .txt document into Pig Latin.", wraplength=450, font=paragraph_font,bg="#3B6869", fg="#ffffff")
+    
+    description_text = tk.Label(root, text="Using this program you are able to seamlessly convert a .txt document into Pig Latin.", wraplength=450, font=paragraph_font, bg="#3B6869", fg="#ffffff")
     description_text.grid(row=2, column=0, sticky="NSEW")
 
-    instruction_text = tk.Label(root, text="To convert a document simply press the button below, select your document and save it to your PC.", wraplength=450, font=paragraph_font,bg="#3B6869", fg="#ffffff")
+    instruction_text = tk.Label(root, text="To convert a document simply press the button below, select your document to be translated and save it to your PC.", wraplength=450, font=paragraph_font, bg="#3B6869", fg="#ffffff")
     instruction_text.grid(row=3, column=0, sticky="NSEW")
 
-    # Convert button
     convert_button = tk.Button(root, text="Select File", command=convert_text, font=button_font)
     convert_button.grid(row=5, column=0, sticky="NSEW")
     convert_button.bind("<Enter>", lambda e: convert_button.config(bg='#e5f0f0', fg="#070d0d"))
     convert_button.bind("<Leave>", lambda e: convert_button.config(bg="#FFFFFF", fg="#3B6869"))
 
-     # Set the main window size and background color
+    # Setting main window size and background color
     root.geometry("600x350")
     root.configure(bg="#3B6869")
 
-    # Start the GUI loop
+    # Starting the GUI loop
     root.mainloop()
 
 if __name__ == "__main__":
